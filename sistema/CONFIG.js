c13d224afa6460c04390f17557693ed7e210445d
asmzonehub <ecomway1@gmail.com>
2026-07-05 01:50:26 -0300

    Add files via upload

---
+++ sistema/CONFIG.js
@@ -0,0 +1,80 @@
+/******************************************************************
+ * CONFIG.js
+ ******************************************************************/
+
+let ASCENSUM = {};
+
+// let WHATSAPP_COMPRAR = "";
+// let WHATSAPP_AYUDA = "";
+// let CONTACTO_PRINCIPAL = "";
+//
+// let TELEGRAM = "";
+//
+// let WALLET = "";
+//
+// let LINK_REFERIDO = "";
+
+async function cargarConfiguracion() {
+
+    try {
+
+        const respuesta = await fetch("../config.json");
+
+        if (!respuesta.ok) {
+            throw new Error("No se pudo cargar config.json");
+        }
+
+        ASCENSUM = await respuesta.json();
+
+        // ===========================
+        // WHATSAPP
+        // ===========================
+
+        WHATSAPP_COMPRAR =
+            "https://wa.link/" + ASCENSUM.whatsapp.comprar;
+
+        WHATSAPP_AYUDA =
+            "https://wa.link/" + ASCENSUM.whatsapp.ayuda;
+
+        CONTACTO_PRINCIPAL =
+            "https://wa.link/" + ASCENSUM.whatsapp.contacto;
+
+        // ===========================
+        // TELEGRAM
+        // ===========================
+
+        TELEGRAM =
+            "https://t.me/" + ASCENSUM.telegram.contacto;
+
+        // ===========================
+        // WALLET
+        // ===========================
+
+        WALLET =
+            ASCENSUM.blockchain.wallet;
+
+        // ===========================
+        // LINK REFERIDO
+        // ===========================
+
+        LINK_REFERIDO =
+            "https://dao.ascensum.io/buy?ref=" +
+            WALLET +
+            "&index=0";
+
+        console.log("✅ Configuración cargada.");
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+
+        cargarConfiguracion();
+
+console.log("CONFIG.js cargado");
+
+
