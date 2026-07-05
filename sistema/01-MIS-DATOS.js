41fa7135b8365a33f1162595cff6bcdd40ad06cd
asmzonehub <ecomway1@gmail.com>
2026-07-04 23:18:21 -0300

    Update 01-MIS-DATOS.js

--- sistema/01-MIS-DATOS.js
+++ sistema/01-MIS-DATOS.js
@@ -16,26 +16,26 @@
 
 /* ===================== CONTACTO ====================== */
 
-const WHATSAPP_COMPRAR = "https://wa.link/eck1xipru";   // Comprar Nodo
+const WHATSAPP_COMPRAR = "https://wa.link/eck1xi";   // Comprar Nodo
 
-const WHATSAPP_AYUDA = "https://wa.link/2nvi78pru";      // Necesito ayuda
+const WHATSAPP_AYUDA = "https://wa.link/2nvi78";      // Necesito ayuda
 
-const CONTACTO_PRINCIPAL = "https://wa.link/gc8hqfpru";  // Barra de navegación
+const CONTACTO_PRINCIPAL = "https://wa.link/gc8hqf";  // Barra de navegación
 
 
 /* ===================== TELEGRAM ====================== */
 
-const TELEGRAM = "https://t.me/nodos_ascensumpru";
+const TELEGRAM = "https://t.me/nodos_ascensum";
 
 
 /* ====================== WALLET ======================= */
 
-const WALLET = "0xB828B26f97245EE744F75Bb3851905C89B0c07A0pru";
+const WALLET = "0xB828B26f97245EE744F75Bb3851905C89B0c07A0";
 
 
 /* ==================== REFERIDO ======================= */
 
-const LINK_REFERIDO = "https://dao.ascensum.io/buy?ref=0xB828B26f97245EE744F75Bb3851905C89B0c07A0&index=0pru";
+const LINK_REFERIDO = "https://dao.ascensum.io/buy?ref=0xB828B26f97245EE744F75Bb3851905C89B0c07A0&index=0";
 
 
 
