b4980e207ecdbc0cb519249c3d5b329ab870d88b
asmzonehub <ecomway1@gmail.com>
2026-07-05 21:13:54 -0300

    Add files via upload

--- sistema/01-MIS-DATOS.js
+++ sistema/01-MIS-DATOS.js
@@ -16,26 +16,26 @@
 
 /* ===================== CONTACTO ====================== */
 
-const WHATSAPP_COMPRAR = "https://wa.link/eck1xi";   // Comprar Nodo
+const WHATSAPP_COMPRAR = "https://wa.link/c9eab2";   // Comprar Nodo
 
-const WHATSAPP_AYUDA = "https://wa.link/2nvi78";      // Necesito ayuda
+const WHATSAPP_AYUDA = "https://wa.link/tikzvz";      // Necesito ayuda
 
-const CONTACTO_PRINCIPAL = "https://wa.link/gc8hqf";  // Barra de navegación
+const CONTACTO_PRINCIPAL = "https://wa.link/sqqlx0";  // Barra de navegación
 
 
 /* ===================== TELEGRAM ====================== */
 
-const TELEGRAM = "https://t.me/nodos_ascensum";
+const TELEGRAM = "https://t.me/nodosasm";
 
 
 /* ====================== WALLET ======================= */
 
-const WALLET = "0xB828B26f97245EE744F75Bb3851905C89B0c07A0";
+const WALLET = "0xB828B26f97245EE744F75Bb3851905C89B0s42F9";
 
 
 /* ==================== REFERIDO ======================= */
 
-const LINK_REFERIDO = "https://dao.ascensum.io/buy?ref=0xB828B26f97245EE744F75Bb3851905C89B0c07A0&index=0";
+const LINK_REFERIDO = "https://dao.ascensum.io/buy?ref=0xB828B26f97245EE744F75Bb3851905C89B0s42F9&index=0";
 
 
 
