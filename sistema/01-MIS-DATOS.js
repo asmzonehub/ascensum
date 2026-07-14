49aba79a519c67207e6368c103f7fcf8374e3231
asmzonehub <ecomway1@gmail.com>
2026-07-13 23:28:45 -0300

    Add files via upload

---
+++ sistema/01-MIS-DATOS.js
@@ -0,0 +1,52 @@
+/* ===========================================================
+                  SISTEMA ASM
+
+           ARCHIVO 1 - MIS DATOS
+
+   Bienvenido.
+
+   Este es el ÚNICO archivo que deberás modificar.
+
+   Solo cambia el texto que aparece entre comillas.
+
+   No modifiques ninguna otra línea.
+
+=========================================================== */
+
+
+/* ===================== CONTACTO ====================== */
+
+const WHATSAPP_COMPRAR = "https://wa.link/eck1xi";   // Comprar Nodo
+
+const WHATSAPP_AYUDA = "https://wa.link/2nvi78";      // Necesito ayuda
+
+const CONTACTO_PRINCIPAL = "https://wa.link/gc8hqf";  // Barra de navegación
+
+
+/* ===================== TELEGRAM ====================== */
+
+const TELEGRAM = "https://t.me/nodos_ascensum";
+
+
+/* ====================== WALLET ======================= */
+
+const WALLET = "0xB828B26f97245EE744F75Bb3851905C89B0c07A0";
+
+
+/* ==================== REFERIDO ======================= */
+
+const LINK_REFERIDO = "https://dao.ascensum.io/buy?ref=0xB828B26f97245EE744F75Bb3851905C89B0c07A0&index=0";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sistema/CONFIG.js
@@ -0,0 +1,93 @@
+/******************************************************************
+ * CONFIG.js
+ ******************************************************************/
+
+let ASCENSUM = {};
+
+ let WHATSAPP_COMPRAR = "";
+ let WHATSAPP_AYUDA = "";
+ let CONTACTO_PRINCIPAL = "";
+
+ let TELEGRAM = "";
+
+ let WALLET = "";
+
+ let LINK_REFERIDO = "";
+
+async function cargarConfiguracion() {
+
+    try {
+
+
+        console.log("Página:", location.href);
+        console.log("Config:", new URL("config.json", location.href).href);
+
+        const respuesta = await fetch("config.json");
+
+
+        if (!respuesta.ok) {
+            throw new Error("No se pudo cargar config.json");
+        }
+
+        ASCENSUM = await respuesta.json();
+
+        console.log(ASCENSUM);
+
+
+
+        // ===========================
+        // WHATSAPP
+        // ===========================
+
+        console.log("Llegué hasta acá");
+
+        WHATSAPP_COMPRAR =
+            "https://wa.link/" + ASCENSUM.whatsapp.comprar;
+
+        WHATSAPP_AYUDA =
+            "https://wa.link/" + ASCENSUM.whatsapp.ayuda;
+
+        CONTACTO_PRINCIPAL =
+            "https://wa.link/" + ASCENSUM.whatsapp.contacto;
+
+        // ===========================
+        // TELEGRAM
+        // ===========================
+
+        TELEGRAM =
+            "https://t.me/" + ASCENSUM.telegram.contacto;
+
+        // ===========================
+        // WALLET
+        // ===========================
+
+        WALLET =
+            ASCENSUM.blockchain.wallet;
+
+        // ===========================
+        // LINK REFERIDO
+        // ===========================
+
+        LINK_REFERIDO =
+            "https://dao.ascensum.io/buy?ref=" +
+            WALLET +
+            "&index=0";
+
+        console.log("✅ Configuración cargada.");
+
+        inicializarEnlaces();
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+
+        cargarConfiguracion();
+
+console.log("CONFIG.js cargado");
+
+
